79ad36ac2aa1a05a46c2120002a19a2f9199d101
stevenlandis <stevenlandis@comcast.net>
2018-12-05 20:11:45 -0800

    write tests

--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
-TEST_CACHE = cache_16c_4a_4b.o
+# TEST_CACHE = cache_16c_4a_4b.o
+# TEST_CACHE = cache_32c_8a_8b.o
+# TEST_CACHE = cache_64c_2a_16b.o
 
 ifndef TEST_CACHE
 $(error You did not define TEST_CACHE!)
@@ -9,3 +11,14 @@ all: cache-test
 
 cache-test: cache-test-skel.c $(TEST_CACHE)
 	gcc -Wall -m32 $^ -o $@
+
+clean:
+	rm -f cache-test
+
+copy:
+
+
+push:
+	git add *
+	git commit - m "auto"
+	git push origin master
--- cache-test-skel.c
+++ cache-test-skel.c
@@ -6,17 +6,17 @@ Due: 12/3/12, 11:00 pm
 
 Mystery Cache Geometries:
 mystery0:
-    block size = 
-    cache size = 
-    associativity = 
+        block size = 
+        cache size = 
+        associativity = 
 mystery1:
-    block size = 
-    cache size = 
-    associativity = 
+        block size = 
+        cache size = 
+        associativity = 
 mystery2:
-    block size = 
-    cache size = 
-    associativity = 
+        block size = 
+        cache size = 
+        associativity = 
 */
 
 #include <stdlib.h>
@@ -25,49 +25,80 @@ mystery2:
 #include "mystery-cache.h"
 
 /* 
-   Returns the size (in B) of the cache
+     Returns the size (in B) of the cache
 */
 int get_cache_size(int block_size) {
-  /* YOUR CODE GOES HERE */
-  
-  return -1;
+    /* YOUR CODE GOES HERE */
+    int i = 0;
+    while (1) {
+        access_cache(i);
+
+        // check previous cache hits to see when overwritten
+        for (int j = 0; j < i; j += block_size) {
+            if (!access_cache(j)) {
+                return i;
+            }
+        }
+
+        i += block_size;
+    }
+    return -1;
 }
 
 /*
-   Returns the associativity of the cache
+     Returns the associativity of the cache
 */
 int get_cache_assoc(int size) {
-  /* YOUR CODE GOES HERE */
-  return -1;
+    // test for cache associacivity:
+    int i = 0;
+    int assoc = 0;
+    while (1) {
+        access_cache(i);
+
+        // check previous cache hits to see when overwritten
+        for (int j = 0; j < i; j += size) {
+            if (!access_cache(j)) {
+                return i;
+            }
+        }
+
+        i += size;
+    }
 }
 
 /*
-   Returns the size (in B) of each block in the cache.
+     Returns the size (in B) of each block in the cache.
 */
 int get_block_size() {
-  /* YOUR CODE GOES HERE */
-  return -1;
+    /* YOUR CODE GOES HERE */
+    flush_cache();
+    access_cache(0);
+    int i = 1;
+    while (access_cache(i)) {
+        i++;
+    }
+    return i;
 }
 
 int main(void) {
-  int size;
-  int assoc;
-  int block_size;
-  
-  /* The cache needs to be initialized, but the parameters will be
-     ignored by the mystery caches, as they are hard coded.
-     You can test your geometry paramter discovery routines by 
-     calling cache_init() w/ your own size and block size values. */
-  cache_init(0,0);
-  
-  block_size = get_block_size();
-  size = get_cache_size(block_size);
-  assoc = get_cache_assoc(size);
+    int size;
+    int assoc;
+    int block_size;
+    
+    /* The cache needs to be initialized, but the parameters will be
+         ignored by the mystery caches, as they are hard coded.
+         You can test your geometry paramter discovery routines by 
+         calling cache_init() w/ your own size and block size values. */
+    cache_init(0,0);
+    
+    block_size = get_block_size();
+    size = get_cache_size(block_size);
+    assoc = get_cache_assoc(size);
 
 
-  printf("Cache size: %d bytes\n",size);
-  printf("Cache associativity: %d\n",assoc);
-  printf("Cache block size: %d bytes\n",block_size);
-  
-  return EXIT_SUCCESS;
+    printf("Cache size: %d bytes\n",size);
+    printf("Cache associativity: %d\n",assoc);
+    printf("Cache block size: %d bytes\n",block_size);
+    
+    return EXIT_SUCCESS;
 }
